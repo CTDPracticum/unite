d13d3c31a60a7d0407f229d8148083d70ab04cdd
Samantha Galindo <bluesundaysg@gmail.com>
2022-09-15 18:29:42 -0400

    Delete function is now working

--- db/schema.rb
+++ db/schema.rb
@@ -59,8 +59,13 @@ ActiveRecord::Schema.define(version: 2022_08_31_225808) do
   end
 
   create_table "memberships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "group_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_memberships_on_group_id"
+    t.index ["user_id", "group_id"], name: "index_memberships_on_user_id_and_group_id", unique: true
+    t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -74,4 +79,6 @@ ActiveRecord::Schema.define(version: 2022_08_31_225808) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "memberships", "groups"
+  add_foreign_key "memberships", "users"
 end
